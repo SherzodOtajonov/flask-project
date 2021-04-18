da2beae7ddb0c632cdb2be557fc71fafda29921d
Sherzod <thisisotajonov@gmail.com>
2021-04-18 19:24:02 +0500

    updates after adding form.py and register.html

--- market/__init__.py
+++ market/__init__.py
@@ -7,6 +7,8 @@ from flask_assets import Bundle, Environment
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SECRET_KEY'] = '244975f26c0ea46be388a170'
+
 db = SQLAlchemy(app)
 assets = Environment(app)
 css = Bundle("src/main.css", output="dist/main.css", filters="postcss")
